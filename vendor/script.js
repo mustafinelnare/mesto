fd03239cd3ead2dba5b09cb86b6aad599dbc9fdf
Эльнар Мустафин <mustafinelnare@gmail.com>
2023-01-17 18:06:50 +0400

    fix popup block

---
+++ vendor/script.js
@@ -0,0 +1,3 @@
+const editBtn = document.querySelector('.profile__edit-button');
+const popupContainer = document.querySelector('.popup');
+const popupCloseBtn = document.querySelector('.popup__close-button');
