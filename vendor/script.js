52d9afc7d1d762e658f6391a3b203cfa34be795d
Эльнар Мустафин <mustafinelnare@gmail.com>
2023-01-17 18:24:10 +0400

    feat open popup

--- vendor/script.js
+++ vendor/script.js
@@ -1,3 +1,20 @@
-const editBtn = document.querySelector('.profile__edit-button');
+const popupOpenBtn = document.querySelector('.profile__edit-button');
 const popupContainer = document.querySelector('.popup');
 const popupCloseBtn = document.querySelector('.popup__close-button');
+
+popupOpenBtn.addEventListener('click', openPopup);
+popupCloseBtn.addEventListener('click', closePopup);
+
+function openPopup() {
+  popupContainer.classList.add('popup__opened');
+}
+
+function closePopup() {
+  popupContainer.classList.remove('popup__opened');
+}
+
+popupContainer.addEventListener('click', function(event) {
+  if (event.target === event.currentTarget) {
+    closePopup();
+  }
+});
