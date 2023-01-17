93c40a672319bc7a9be465e7ff1177e13b6362ac
Эльнар Мустафин <mustafinelnare@gmail.com>
2023-01-17 23:02:41 +0400

    feat js file popup

--- vendor/script.js
+++ vendor/script.js
@@ -1,6 +1,13 @@
-const popupOpenBtn = document.querySelector('.profile__edit-button');
-const popupContainer = document.querySelector('.popup');
-const popupCloseBtn = document.querySelector('.popup__close-button');
+let popupOpenBtn = document.querySelector('.profile__edit-button');
+let popupContainer = document.querySelector('.popup');
+let popupCloseBtn = document.querySelector('.popup__close-button');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+
+/* Открытие и закрытие */
 
 popupOpenBtn.addEventListener('click', openPopup);
 popupCloseBtn.addEventListener('click', closePopup);
@@ -13,8 +20,37 @@ function closePopup() {
   popupContainer.classList.remove('popup__opened');
 }
 
+/* Выход из попапа ткнув в пустое место */
+
 popupContainer.addEventListener('click', function(event) {
   if (event.target === event.currentTarget) {
     closePopup();
   }
 });
+
+/* Редактирование имени и информации о себе */
+
+popupOpenBtn.addEventListener('click', function() {
+  openPopup(popupContainer);
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+});
+
+popupCloseBtn.addEventListener('click', function() {
+  closePopup(popupContainer);
+});
+
+popupContainer.addEventListener("click", function (evt) {
+  if (evt.target === evt.currentTarget) {
+    closePopup(popupContainer);
+  }
+});
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+  closePopup(popupContainer);
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
